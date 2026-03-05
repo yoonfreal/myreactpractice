be438d75cdb6818b94e4351f69f672fdac595580
yoonfreal <u6642005@au.edu>
2026-03-05 15:47:23 +0700

    commit3

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,11 @@
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import { Items } from './Items';
-import { ItemDetail } from './ItemDetail';
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { Items } from "./Items";
+import { ItemDetail } from "./ItemDetail";
 
 function App() {
   return (
-    <Router>
-      <div style={{ padding: '20px' }}>
+    <Router basename="/myreactpractice_deploy">
+      <div style={{ padding: "20px" }}>
         <Routes>
           <Route path="/" element={<Items />} />
           <Route path="/items/:id" element={<ItemDetail />} />
